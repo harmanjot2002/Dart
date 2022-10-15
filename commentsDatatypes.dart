3981c4dccd32e2278ad55f7b72484c067620b1c4
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-15 11:41:27 +0530

    Comments and datatypes

---
+++ commentsDatatypes.dart
@@ -0,0 +1,42 @@
+/*
+  Single line comments using//
+  multiline comments using /*  */
+  documentation comments using ///
+*/
+
+/*
+main(){
+  print("Harman");
+  print("Jot");
+  //print("Kaur");
+}
+*/
+
+/*There are 7 datatypes in dart
+  1.int
+  2.double
+  3.string
+  4.boolean
+  5.list
+  6.map
+  7.dynamic
+*/
+
+main(){
+  int age=10;
+  bool roll=true;
+  String name="Harman";
+  double lage=10.10;
+  List arr=[1,2,"Vishal"];
+  Map arr1={"name":"Harman","age":20};
+  var dyn=10;
+
+  print(age);
+  print(roll);
+  print(name);
+  print(lage);
+  print(arr);
+  print(arr[2]);
+  print(arr1);
+  print(dyn);
+}
