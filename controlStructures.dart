393d5cc15c7386f5db98781b5c47095b57007317
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-16 13:13:10 +0530

    Control Structures

---
+++ controlStructures.dart
@@ -0,0 +1,25 @@
+/*Control Structures:
+  1.Decision making Statements
+    a)if-elseif-else ladder
+    b)switch case
+  2.Looping Statements
+    a)for loop
+    b)while loop
+    c)do while loop
+  3.Jump Statements
+    a)break
+    b)continue
+*/
+
+//Decision making statements
+main(){
+  var name='Vishal';
+  if(name=='Vishal')
+    print('right');
+  else if(name=='Ajay')
+    print('wrong');
+  else
+    print('None');
+}
+
+
