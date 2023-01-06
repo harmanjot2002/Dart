2e6c37d6bb8fbdf3cb4934fdf6ddf476f5df75e9
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-06 20:43:51 +0530

    Free Code Camp

--- freeCodeCampOrg.dart
+++ freeCodeCampOrg.dart
@@ -162,12 +162,48 @@ assert(piAsString == '3.14');
   print(result);
 */
 
+/*
   //Type Test
   var x=100;
   if(x is int){
     print("Integer");
   }
+*/
 
+/*
   //Conditional Statement
+  int number = 100;
+  if (number % 2 == 0) {
+    print('Even');
+  }
+  else if (number % 3 == 0) {
+    print('Odd');
+  }
+  else {
+    print('Confused');
+  }
+
+  int number = 1;
+  switch(number) {
+    case 0:
+      print('Even');
+      break;
+    case 1:
+      print('Odd');
+      break;
+    default:
+      print('Confused');
+  }
+*/
+
+//Loops
+for (var i = 0; i < 10; ++i) {
+  print(i);
+}
+var numbers = [1, 2, 3];
+for (var n in numbers) {
+  print(n);
+}
+numbers.forEach((num) => print(num));
 
 }
