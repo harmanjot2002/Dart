802b8dd1ffa30d908f309788e22a7f3992a0a446
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-06 09:06:20 +0530

    Free Code Camp

--- freeCodeCampOrg.dart
+++ freeCodeCampOrg.dart
@@ -1,5 +1,17 @@
 main(){
   //JIT-Just In Time-at run time
   //AOT-Ahead Of Time-compile time
-  
+
+
+  var firstName = 'Mahmud'; // String type inference
+  String lastName = 'Ahsan'; // String type defined
+  int number = 100; // integer type
+  double cost = 11.40;
+  dynamic isOkay = true; // dynamic type can holds any type
+  print(firstName + ' ' + lastName);
+  print(number);
+  print(cost);
+  print(isOkay);
+
+
 }
