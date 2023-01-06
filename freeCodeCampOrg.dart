f42aa3545715c34ecec4577ab157f138960b1bbd
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-06 19:15:08 +0530

    Free Code Camp

--- freeCodeCampOrg.dart
+++ freeCodeCampOrg.dart
@@ -1,4 +1,6 @@
-main(){
+import 'dart:io';
+
+void main(){
   //JIT-Just In Time-at run time
   //AOT-Ahead Of Time-compile time
 
@@ -14,4 +16,34 @@ main(){
   print(isOkay);
 
 
+  stdout.writeln('What is your name: ?');
+  String? name = stdin.readLineSync();
+  print('My name is: $name');
+
+  /*
+    Simple or Primitive Data Types
+      int
+      double
+      String
+      bool
+      dynamic
+  */
+  int amount1 = 100;
+  var amount2 = 200;
+  print('Amount1: $amount1 | Amount2: $amount2 \n');
+  double dAmount1 = 100.11;
+  var dAmount2    = 200.22;
+  print('dAmount1: $dAmount1 | dAmount2: $dAmount2 \n');
+  String name1 = 'Mahmud';
+  var name2    = 'Ahsan';
+  print('My name is: $name1 $name2 \n');
+  bool isItTrue1 = true;
+  var isItTrue2  = false;
+  print('isItTrue1: $isItTrue1 | isItTrue2: $isItTrue2 \n');
+  dynamic weakVariable = 100;
+  print('WeakVariable 1: $weakVariable \n');
+  weakVariable = 'Dart Programming';
+  print('WeakVariable 2: $weakVariable');
+
+  
 }
